1e94274f91ebcc3c2fde3c8ef557bdc2ddeb077e
Jignesh Makwana <makwanajigneshm@gmail.com>
2024-02-20 21:45:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as sl
+import pandas as p
 sl.write("Hello")
 sl.header("New World")
 sl.text("Welcome to the party!")
-
+my_fruit_list=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.datagrame(my_fruit_list)
