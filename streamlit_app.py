8d4b990dc1fb50837c0d7524e1a4e70588717ac1
Jignesh Makwana <makwanajigneshm@gmail.com>
2024-02-20 21:48:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ sl.header("New World")
 sl.text("Welcome to the party!")
 my_fruit_list=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 sl.dataframe(my_fruit_list)
+sl.multiselect("Pick some Fruits",list(my_fruit_list.index))
+
+sl.dataframe(my_fruit_list)
+
