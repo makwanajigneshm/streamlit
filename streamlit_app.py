35c85ae6b71dfc1b7c5563d71b78e4f90c4bd476
Jignesh Makwana <makwanajigneshm@gmail.com>
2024-02-20 21:59:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ my_fruit_list=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/
 my_fruit_list=my_fruit_list.set_index("Fruit")
 fruits_selected=sl.multiselect("Pick some Fruits",list(my_fruit_list.index),["Apple","Cantaloupe"])
 sl.dataframe(my_fruit_list)
-chosen_fruits=my_fruit_list.loc(fruits_selected)
+chosen_fruits=my_fruit_list.loc[fruits_selected]
 sl.dataframe(chosen_fruits)
 
 
